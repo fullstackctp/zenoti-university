e93c15dd0ba11b398461d2342d60ecff56f981c4
fullstackctp <fullstackctp@gmail.com>
2021-07-01 14:28:22 +0530

    Gift card single page commit.

---
+++ js/main.js
@@ -0,0 +1,54 @@
+// overlay card-1
+$(document).ready(function () {
+  $(".trigger1").click(function () {
+    $("#overlay1").show();
+  });
+
+  $("#close1").click(function () {
+    $("#overlay1").fadeOut(300);
+  });
+});
+
+// overlay card-2
+$(document).ready(function () {
+  $(".trigger2").click(function () {
+    $("#overlay2").show();
+  });
+
+  $("#close2").click(function () {
+    $("#overlay2").fadeOut(300);
+  });
+});
+
+// overlay card-3
+$(document).ready(function () {
+  $(".trigger3").click(function () {
+    $("#overlay3").show();
+  });
+
+  $("#close3").click(function () {
+    $("#overlay3").fadeOut(300);
+  });
+});
+
+// overlay card-4
+$(document).ready(function () {
+  $(".trigger4").click(function () {
+    $("#overlay4").show();
+  });
+
+  $("#close4").click(function () {
+    $("#overlay4").fadeOut(300);
+  });
+});
+
+// 5
+$(document).ready(function () {
+  $(".trigger5").click(function () {
+    $("#overlay5").show();
+  });
+
+  $("#close5").click(function () {
+    $("#overlay5").fadeOut(300);
+  });
+});
